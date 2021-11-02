ade0dd729ac511d9baa73ea642a17bb3f2e11da8
Vika <Vika@HOME-PC>
2021-11-03 01:28:53 +0300

    Добавьте файлы проекта.

---
+++ kolok_ex10/kolok_ex10.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <math.h>
+int main() {
+	int m;
+	std::cin >> m;
+	int s = 0;
+	int x;
+	std::cin >> x;
+	for (int n = 1; n <= m; n++) {
+		s += (pow(x, 2 * n - 1)/(2*n - 1) * sin(2 * n * x));
+	}
+	std::cout << s;
+	return 0;
+}
